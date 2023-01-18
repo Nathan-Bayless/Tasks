b0199eee83175c3aa707e1924e3baf297e205cd6
Nathan-Bayless <nathansbayless@gmail.com>
2023-01-18 10:07:25 -0500

    <Nathan-Bayless> Task 01 upload c

--- Task_01/task01.r
+++ Task_01/task01.r
@@ -1,3 +1,36 @@
-# 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
+# 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
 library(swirl)
 swirl()
+Nate
+1
+1
+1
+1
+1
+5+7
+x <- 5 + 7
+x
+y <- x - 3
+y
+z <- c(1.1, 9, 3.14)
+?c
+z
+c(555)z
+c(555) z
+c(555)
+c(z, 555, z)
+z * 2 + 1000
+my_sqrt <- sqrt(z - 1)
+3
+my_sqrt
+my_div <- z / my_sqrt
+3
+my_div
+c(1, 2, 3, 4) / c(0, 10)
+c(1, 2, 3, 4) + c(0, 10)
+c(1, 2, 3, 4) + c(0, 10, 100)
+my_div
+2
+nathansbayless@gmail.com
+1
+2
