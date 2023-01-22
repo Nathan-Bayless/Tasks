335c47b3d9dc16dbc7e5d2f9fcfa97bd92ff018f
Nathan-Bayless <nathansbayless@gmail.com>
2023-01-22 15:48:09 -0500

    <Nathan-Bayless> Task 02 upload a

---
+++ Task_02/Task 02.R
@@ -0,0 +1,32 @@
+setwd('/Users/nathanbayless/Desktop/Evolution/Tasks/Task_02')
+Data1 <- read.csv('http://jonsmitchell.com/data/beren.csv', stringsAsFactors = F)
+Data2 <- read.csv('http://jonsmitchell.com/data/cyrus.csv', stringsAsFactors = F)
+write.csv(Data1, 'rawdata.csv', quote = F)
+Data1
+length(Data1)
+nrow(Data1)
+ncol(Data1)
+colnames(Data1)
+head(Data1)
+Data1[1,]
+Data1[2,]
+Data1[1:3,]
+Data1[1:3, 4]
+Data1[1:5, 1:3]
+Data1[257,]
+Feeds <- which(Data1[,9]=='bottle')
+berenMilk <- Data1[Feeds,]
+head(berenMilk)
+Feeds <- which(Data1[,'event']=='bottle')
+berenMilkk <- Data1[Feeds,]
+head(berenMilkk)
+dayID <- apply(Data1, 1, function(x) paste(x[1:3], collapse = '-'))
+dateID <- sapply(dayID, as.Date, format = "%Y-%m-%d", origin = "2019-04-18")
+Data1$age <- dateID - dateID[which(Data1$event == 'birth')]
+head(Data1)
+beren2 <- Data1
+beren3 <- beren2[order(beren2$age),]
+write.csv(beren3, 'beren_new.csv', quote = F, row.names = FALSE)
+Feeds <- which(beren3$event == 'bottle')
+avgMilk <- mean(beren3$value[Feeds])
+avgFeed <- tapply(beren3$value[Feeds], beren3$age[Feeds], mean)
